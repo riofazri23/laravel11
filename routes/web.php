005608ce1dcca0d8dcfc92ecb920ad62e1a7e2cb
Rio Fazri Septian <rfazri7@gmail.com>
2024-05-13 16:06:29 +0700

    view data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home',['title'=>'Home Page']);
@@ -15,12 +16,53 @@ Route::get('/about', function(){
     );
 });
 
-Route::get('/blog',function(){
-    return view('blog',
+Route::get('/posts',function(){
+    return view('posts',
         [
-            'title'=>'Blog'
+            'title'=>'Post',
+            'posts'=>[
+                [
+                    'id'=>1,
+                    'slug'=>'judul-artikel-1',
+                    'title'=>'Judul Artikel 1',
+                    'author'=>'Rio Fazri',
+                    'body'=>'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto voluptas mollitia similique quis eaque impedit quisquam quaerat repudiandae eos saepe.'
+                ],
+                [
+                    'id'=>2,
+                    'slug'=>'judul-artikel-2',
+                    'title'=>'Judul Artikel 2',
+                    'author'=>'Rio Fazri',
+                    'body'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure eaque autem possimus dolor ex beatae reprehenderit. Sint ducimus fugiat totam rerum. Minus voluptates deserunt dolore quisquam voluptatem, amet adipisci quaerat?'
+                ]
+            ]
         ]
     );
+});
+
+Route::get('/posts/{slug}',function($slug){
+    $posts=[
+            [
+                'id'=>1,
+                'title'=>'Judul Artikel 1',
+                'slug'=>'judul-artikel-1',
+                'author'=>'Rio Fazri',
+                'body'=>'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto voluptas mollitia similique quis eaque impedit quisquam quaerat repudiandae eos saepe.'
+            ],
+            [
+                'id'=>2,
+                'title'=>'Judul Artikel 2',
+                'slug'=>'judul-artikel-2',
+                'author'=>'Rio Fazri',
+                'body'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure eaque autem possimus dolor ex beatae reprehenderit. Sint ducimus fugiat totam rerum. Minus voluptates deserunt dolore quisquam voluptatem, amet adipisci quaerat?'
+            ]
+        ];
+
+        $post = Arr::first($posts, function($post) use ($slug) {
+            return $post['slug'] == $slug;
+        });
+
+        return view('post',['title'=>'Single Post', 'post'=>$post]);
 });
 
 Route::get('/contact',function(){
